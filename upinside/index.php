3b9220b0945b019ce1860ce3221bb9e7c947ffca
Thiago Almeida <ts.analista@gmail.com>
2018-03-12 22:09:32 -0300

    correction attr border-radius style.sass

--- upinside/index.php
+++ upinside/index.php
@@ -27,18 +27,7 @@ define('BASE', 'http://upinside.test');
     </head>
 
     <body>
-<div class="trigger trigger-accept">
-    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
-</div>
-<div class="trigger trigger-alert">
-<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
-</div>
-<div class="trigger trigger-infor">
-<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
-</div>
-<div class="trigger trigger-error">
-<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
-</div>
+
 
         <header class="container bg-gray">
             <div class="content">
@@ -69,7 +58,7 @@ define('BASE', 'http://upinside.test');
                         <a href="<?=BASE;?>/micro-dados" title="Distribuição com Micro Dados">Micro Dados</a>
                     </li>
                     <li>
-                        <a href="<?=BASE;?>/sobre-curso" class="btn btn-yellow radius boxshadow" title="Sobre o Curso">Curso</a>
+                        <a href="<?=BASE;?>/sobre-curso" class="btn btn-green radius boxshadow" title="Sobre o Curso">Curso</a>
                     </li>
                    
                 </ul>
