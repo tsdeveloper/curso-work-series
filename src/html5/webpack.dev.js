009ed59d10fdbfe9b1b0e8765030d02c68bcf356
Build Code Developer <build.code.developer@gmail.com>
2018-12-30 00:25:49 -0200

    Add Devtools

--- src/html5/webpack.dev.js
+++ src/html5/webpack.dev.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    // mode: 'development',
+    mode: 'development',
     devtool: 'inline-source-map',
-
+    devServer: {
+        contentBase: './public',
+        host: 'http://work-series-html5.test',
+        port: 8081,
+        index: 'index.php'
+    }
 });
